a9e05350a5eee0fde61b7b9d0cb2891b07ae9ced
Abraham Tsang <abraham.tsang@gmail.com>
2021-03-28 11:30:36 +0800

    collecting translations

--- main.js
+++ main.js
@@ -1,3 +1,9 @@
 const fs = require('fs');
 
-var 
+var translations = [];
+translations.push(fs.readFileSync('sv-en.txt', {encoding: 'utf8'}));
+translations.push(fs.readFileSync('ja-en.txt', {encoding: 'utf8'}));
+translations.push(fs.readFileSync('pt-en.txt', {encoding: 'utf8'}));
+translations.push(fs.readFileSync('ch-en.txt', {encoding: 'utf8'}));
+
+console.log(translations)
